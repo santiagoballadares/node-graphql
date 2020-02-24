5f9589ecef9047b305afc62280830df5502c597b
Santiago Balladares <santiago.balladares@outlook.com>
2020-02-23 21:07:57 -0500

    Added filtering, pagination, and sorting.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,40 @@
-const { getUserId } = require('../utils');
-
 const info = () => `This is the API of a GraphQL Node project.`;
 
-const feed = (root, args, context, info) => {
-  const userId = getUserId(context);
-  return context.prisma.links();
+const feed = async (root, args, context, info) => {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.linksConnection({
+    where,
+    skip: args.skip,
+    first: args.first,
+  })
+  .aggregate()
+  .count();
+
+  return {
+    links,
+    count,
+  };
+};
+
+const link = async (root, args, context, info) => {
+  return await context.prisma.link({ id: args.id });
 };
 
 module.exports = {
   info,
   feed,
+  link,
 };
